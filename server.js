9cef97c4741e07d3efdf28abac318756acc9b6a3
Nick <nrodrig1@gmail.com>
2024-11-27 23:24:34 +0100

    log app name

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const app = require("./app"); // Import the configured app
 const PORT = process.env.PORT || 3000;
 
+// const APP_NAME = process.env.APP_NAME;
+const APP_NAME = "process.env.APP_NAME";
+console.log = (
+  (log) => (message) =>
+    log(`[${APP_NAME}] ${message}`)
+)(console.log);
+console.error = (
+  (log) => (message) =>
+    log(`[${APP_NAME}] ${message}`)
+)(console.error);
+
 // Start the server
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
